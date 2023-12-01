43615c2867a27e66c9810c8b584f2683206eb343
Daniel Yoo <dacapo0509@gmail.com>
2023-12-01 13:56:13 -0800

    start of portfolio using vite and scss

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./app.scss";
+
+const App = () => {
+	return (
+		<div>
+			<section>Hero</section>
+			<section>Parallax</section>
+			<section>Work Experience</section>
+			<section>Parallax</section>
+			<section>Portfolio 1</section>
+			<section>Portfolio 2</section>
+			<section>Portfolio 3</section>
+			<section>Portfolio 4</section>
+			<section>Contact</section>
+		</div>
+	);
+};
+
+export default App;
